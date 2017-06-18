ccace400f61fd928f27e6bc0a1df380ed1ab1c9a
Alejandro Rivas <jarlalejor@gmail.com>
2017-06-17 22:27:11 -0500

    inicio

--- home/cards.php
+++ home/cards.php
@@ -1,29 +1,25 @@
 <?php
 $id = $_SESSION["3188768e18a5c00164bbe22d1749a30e4626a114"];
 
+$qs = $pdo->query("SELECT * FROM sgmeempl WHERE eempl_ced_eempl='$id'");
+$employee = $qs->fetch();
+
+$rol = strtoupper($employee['eempl_car_eempl']);
+
+if ($rol == 'TECNICO') {
+  require "./tecnico.php";
+} else if ($rol == 'MANTENIMIENTO') {
+  require "./manteniento.php";
+} else if ($rol == 'JEFES') {
+  require "./jefes.php";
+}
+
+/*
 $ordenes = $pdo->query("SELECT * FROM sgmeorin WHERE eorin_est_eorin='asignado'
 	AND eorin_emp_eorin='$id'");
 $countOrden = $ordenes->rowCount();
 
 $task = $pdo->query("SELECT * FROM sgmetare WHERE etare_est_etare='asginado' AND etare_emp_etare='$id'");
 $countTask = $task->rowCount();
+*/
 ?>
-<article class="col-xs-10 col-md-5 cardHome">
-	<?php
-		if ($countTask == 0) {
-			echo '<a>No Tienes Tareas pendientes</a>';
-		}
-		else {
-			echo '<a class="activeHome">Tienes '.$countTask.' Tareas pendientes</a>';
-		}
-	?>
-</article>
-<article class="col-xs-10 col-md-5 cardHome">
-<?php
- 	if ($countOrden == 0) {
- 		echo '<a href="#">No Tienes O.T Interna pendientes</a>';
- 	}else {
- 		echo '<a href="../mis-ordenes-trabajo-interno/" class="activeHome">Tienes '.$countOrden.' O.T Interna pendientes</a>';
- 	}
- ?>
-</article>
